2e4b4727d426b5f58f2f618d37f68730ccf7dc4a
Kashif Mahmood <31515792+intelligentCoding@users.noreply.github.com>
2020-03-10 14:19:29 -0400

    Finished and Tested

--- readme.md
+++ readme.md
@@ -5,5 +5,5 @@ Form Validation using Vanilla JavaScript.
 ## Steps
 
 - Created the form and applied CSS to it.
-- Basic Validation
-
+- refactored all the if conditions
+- functions to check email and password validation
--- script.js
+++ script.js
@@ -19,10 +19,14 @@ function showSuccess(input){
 	formControl.className = 'form-control success';
 }
 //check email validation
-function validEmail(email){
+function checkEmail(input){
 	const regix = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	//use test method to check if it is valid
-    return regix.test(String(email).toLowerCase());
+    if(regix.test(input.value.trim())){
+		showSuccess(input)
+	}else {
+		showError(input, 'Email is not valid');
+	}
 }
 
 function getFieldName(input){
@@ -40,10 +44,32 @@ function checkRequired(inputArr){
 		}
 	});
 }
+
+// check length
+function checkLength(input, min, max){
+	if(input.value.length < min){
+		showError(input, `${getFieldName(input)} must be atleast ${min} characters`);
+	} else if (input.value.length > max){
+		showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+	} else {
+		showSuccess(input);
+	}
+}
+//check password match
+function checkpasswordsMatch(input1, input2){
+	if(input1.value != input2.value){
+		showError(input2, "Password do not match");
+	}
+}
+
 //add event listnet
 form.addEventListener('submit', function (e){
 	//stop the form from submitting
 	e.preventDefault();
 	//send an array to inputs to the function
 	checkRequired([username, email, password, password2]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
+	checkEmail(email);
+	checkpasswordsMatch(password, password2);
 })
